bfd03450e629a3a3a5dc9f3c3bfb9a920a89f2f9
tkoyama010/pyvista-docs-dev-ja <tkoyama010/pyvista-docs-dev-ja>
2022-01-22 02:14:10 +0000

    [ci skip] 4904214591?check_suite_focus=true https://github.com/tkoyama010/pyvista-docs-dev-ja/runs/4904214591?check_suite_focus=true

--- api/core/_autosummary/pyvista-ExplicitStructuredGrid-find_closest_cell-1.py
+++ api/core/_autosummary/pyvista-ExplicitStructuredGrid-find_closest_cell-1.py
@@ -28,3 +28,30 @@ indices = mesh.find_closest_cell(points)
 indices.shape
 # Expected:
 ## (5000,)
+#
+# For the closest cell, find the point inside the cell that is
+# closest to the supplied point.  The rectangle is a unit square
+# with 1 cell and 4 nodal points at the corners in the plane with
+# ``z`` normal and ``z=0``.  The closest point inside the cell is
+# not usually at a nodal point.
+#
+unit_square = pyvista.Rectangle()
+index, closest_point = unit_square.find_closest_cell(
+    [0.25, 0.25, 0.5],
+    return_closest_point=True
+)
+closest_point
+# Expected:
+## array([0.25, 0.25, 0.  ])
+#
+# But, the closest point can be a nodal point, although the index of
+# that point is not returned.  If the closest nodal point by index is
+# desired, see :func:`DataSet.find_closest_point`.
+#
+index, closest_point = unit_square.find_closest_cell(
+    [1.0, 1.0, 0.5],
+    return_closest_point=True
+)
+closest_point
+# Expected:
+## array([1., 1., 0.])
