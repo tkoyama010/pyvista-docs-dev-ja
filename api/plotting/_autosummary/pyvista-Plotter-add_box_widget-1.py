b2667a8d9e292dae3924e90aa68d6b9a5e0a6718
tkoyama010/pyvista-docs-dev-ja <tkoyama010/pyvista-docs-dev-ja>
2022-05-11 03:33:23 +0000

    [ci skip] 6381153787 https://github.com/tkoyama010/pyvista-docs-dev-ja/runs/6381153787?check_suite_focus=true

--- api/plotting/_autosummary/pyvista-Plotter-add_box_widget-1.py
+++ api/plotting/_autosummary/pyvista-Plotter-add_box_widget-1.py
@@ -1,8 +1,7 @@
-# The following example generates a static image of the widget.
+# Shows an interactive clip box.
 #
 import pyvista as pv
-from pyvista import examples
-mesh = examples.download_nefertiti()
-p = pv.Plotter()
-_ = p.add_mesh_clip_box(mesh, color='white')
-p.show(cpos=[-1, -1, 0.2])
+mesh = pv.ParametricConicSpiral()
+pl = pv.Plotter()
+_ = pl.add_mesh_clip_box(mesh, color='white')
+pl.show()
