ba930eb4934658de79d7b583110df8ea429fe3ab
tkoyama010/pyvista-docs-dev-ja <tkoyama010/pyvista-docs-dev-ja>
2022-06-03 03:24:23 +0000

    [ci skip] 6719451125 https://github.com/tkoyama010/pyvista-docs-dev-ja/runs/6719451125?check_suite_focus=true

--- api/plotting/_autosummary/pyvista-Plotter-export_html-1.py
+++ api/plotting/_autosummary/pyvista-Plotter-export_html-1.py
@@ -1,3 +1,5 @@
+# Export as a three.js scene using the pythreejs backend.
+#
 import pyvista
 from pyvista import examples
 mesh = examples.load_uniform()
@@ -6,3 +8,7 @@ _ = pl.add_mesh(mesh, scalars='Spatial Point Data', show_edges=True)
 pl.subplot(0,1)
 _ = pl.add_mesh(mesh, scalars='Spatial Cell Data', show_edges=True)
 pl.export_html('pyvista.html')  # doctest:+SKIP
+#
+# Export as a vtk.js scene using the panel backend.
+#
+pl.export_html('pyvista_panel.html', backend='panel')  # doctest:+SKIP
